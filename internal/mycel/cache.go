b114cabb2a3cca28628823a9ffc8b88d87e823ff
conamu <constantin.amundsen@ho-me.zone>
2025-11-15 14:40:29 +0100

    functions and DS setup to get data

--- internal/mycel/cache.go
+++ internal/mycel/cache.go
@@ -1,18 +1,48 @@
 package mycel
 
 import (
+	"errors"
+	"fmt"
 	"time"
 )
 
 type Cache interface {
+	CreateBucket(name string, ttl time.Duration, maxLen int) error
 	Get(bucket, key string) (any, error)
 	Put(bucket, key string, value any, ttl time.Duration) error
 	Delete(bucket, key string) error
 	PutTtl(bucket, key string, ttl time.Duration) error
 }
 
-func (c *cache) Get(bucket, key string) (any, error) {
+func (c *cache) CreateBucket(name string, ttl time.Duration, maxLen int) error {
+	c.lruBuckets.Lock()
+	defer c.lruBuckets.Unlock()
 
+	if c.lruBuckets.data[name] != nil {
+		return errors.New("bucket already exists")
+	}
+
+	c.lruBuckets.data[name] = &lruBucket{
+		maxLen: maxLen,
+	}
+	return nil
+}
+
+func (c *cache) Get(bucket, key string) (any, error) {
+	// This means we have the data available locally
+	if n := c.keyVal.Get(bucket + key); n != nil {
+		n.RLock()
+		defer n.RUnlock()
+
+		if b := c.lruBuckets.GetBucket(bucket); b != nil {
+			c.lruBuckets.RUnlock()
+			b.Push(n)
+			return n.data, nil
+		}
+
+		return nil, errors.New(fmt.Sprintf("no bucket with name %s", bucket))
+	}
+	return nil, errors.New("not found")
 }
 
 func (c *cache) Put(bucket, key string, value any, ttl time.Duration) error {
@@ -28,4 +58,9 @@ func (c *cache) Delete(bucket, key string) error {
 func (c *cache) PutTtl(bucket, key string, ttl time.Duration) error {
 	//TODO implement me
 	panic("implement me")
+}
+
+// shutdown will gracefully mark the node as dead and sign off from the network
+func (c *cache) shutdown() {
+
 }
