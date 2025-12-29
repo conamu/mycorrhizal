f93b33389c144dcda1ad1ab14056825347aec1b0
conamu <constantin.amundsen@ho-me.zone>
2025-12-29 13:15:21 +0100

    remote get and set complete

--- internal/mycel/remote.go
+++ internal/mycel/remote.go
@@ -4,25 +4,47 @@ import (
 	"time"
 )
 
-func (c *cache) getRemote(bucket, key string) ([]byte, error) {
+func (c *cache) getRemote(bucket, key string) (any, error) {
 	c.nodeScoreHashMap.RLock()
 	target := c.nodeScoreHashMap.data[bucket+key]
 	c.nodeScoreHashMap.RUnlock()
 
-	res, err := c.app.Request(nil, target, time.Millisecond*100)
+	payload, err := c.gobEncode(remoteCachePayload{
+		Operation: GET,
+		Key:       key,
+		Bucket:    bucket,
+	})
 	if err != nil {
 		return nil, err
 	}
-	return res, nil
+
+	res, err := c.app.Request(payload, target, time.Millisecond*100)
+	if err != nil {
+		return nil, err
+	}
+
+	val, err := c.gobDecode(res)
+	if err != nil {
+		return nil, err
+	}
+
+	return val.Value, nil
 }
 
 func (c *cache) setRemote(bucket, key string, value any, ttl time.Duration) error {
 
-	buf := make([]byte, 1)
+	payload, err := c.gobEncode(remoteCachePayload{
+		Operation: SET,
+		Key:       key,
+		Bucket:    bucket,
+		Value:     value,
+		Ttl:       ttl,
+	})
+	if err != nil {
+		return err
+	}
 
-	buf[0] = SET
-
-	res, err := c.app.Request(buf, c.nodeScoreHashMap.data[bucket+key], ttl)
+	res, err := c.app.Request(payload, c.nodeScoreHashMap.data[bucket+key], time.Millisecond*100)
 	if err != nil {
 		return err
 	}
